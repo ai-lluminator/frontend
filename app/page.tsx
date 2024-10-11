9488a8accbccc55ad06bdf99ea200b5d2e0aa3ea
Tom Härter <tom@dokedu.org>
2024-10-11 18:27:55 +0200

    Refactor redirect logic to use Next.js navigation and update app metadata for AI/lluminator

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,5 @@
-"use client";
+import { redirect } from 'next/navigation';
 
-import Link from 'next/link';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import React from 'react';
-
-const RedirectToLogin = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Programmatically navigate to /login
-    router.push('/home');
-  }, [router]);
-
-  return (
-    <div>
-    <title>Redirecting...</title>
-      <p>Redirecting to login...</p>
-      <p>
-        If you are not redirected, click <Link href="/home">here</Link>.
-      </p>
-    </div>
-  );
-};
-
-export default RedirectToLogin;
+export default function RedirectToLogin() {
+  redirect('/home');
+}
